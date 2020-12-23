3ba9a48f8767df105c218edad1f8896443563e7f
JMustang <eecfredes@gmail.com>
2020-12-22 23:31:03 -0300

    a instagram clone

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import * as firebase from "firebase";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyABzFuzl_6y_Hf3rB0jf0_i33RBzDVbCbM",
+  authDomain: "instagram-clone-d7a84.firebaseapp.com",
+  projectId: "instagram-clone-d7a84",
+  storageBucket: "instagram-clone-d7a84.appspot.com",
+  messagingSenderId: "5168269654",
+  appId: "1:5168269654:web:e1d5ea84d47c810b8feef7",
+  measurementId: "G-NFDJXDHNV4",
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import LandingScreen from "./components/auth/Landing";
+import RegisterScreen from "./components/auth/Register";
+
+const Stack = createStackNavigator();
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Landing">
+        <Stack.Screen
+          name="Landing"
+          component={LandingScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
---
+++ comandos.md
@@ -0,0 +1,9 @@
+# Comandos usados nesse projeto
+
+expo init instagram-clone
+yarn add @react-navigation/native
+yarn add @react-navigation/stack
+expo install firebase
+yarn add react-native-screens
+yarn add @react-native-community/masked-view
+yarn add react-native-gesture-handler
