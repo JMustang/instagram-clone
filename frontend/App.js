4f16a74bd527012441b48e7373071a7a713d2439
JMustang <eecfredes@gmail.com>
2020-12-25 21:38:42 -0300

    feat:navigator-tabs implemented

--- frontend/App.js
+++ frontend/App.js
@@ -29,7 +29,9 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import LandingScreen from "./components/auth/Landing";
 import RegisterScreen from "./components/auth/Register";
+import LoginScreen from "./components/auth/Login";
 import MainScreen from "./components/Main";
+import AddScreen from "./components/main/Add";
 
 const Stack = createStackNavigator();
 
@@ -75,13 +77,23 @@ export class App extends Component {
               options={{ headerShown: false }}
             />
             <Stack.Screen name="Register" component={RegisterScreen} />
+            <Stack.Screen name="Login" component={LoginScreen} />
           </Stack.Navigator>
         </NavigationContainer>
       );
     }
     return (
       <Provider store={store}>
-        <MainScreen />
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen
+              name="Main"
+              component={MainScreen}
+              options={{ headerShown: false }}
+            />
+            <Stack.Screen name="Add" component={AddScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </Provider>
     );
   }
